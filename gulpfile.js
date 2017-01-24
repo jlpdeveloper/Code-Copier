77c3b71baf910a79d298387d668b247459c8f9d8
jlpdeveloper <pottsman482@gmail.com>
2017-01-23 19:32:20 -0600

    added build js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 var gulp = require('gulp');
-var  childProcess  = require('child_process'), 
-  electron      = require('electron-prebuilt');
+var childProcess = require('child_process'),
+  electron = require('electron-prebuilt'),
+  concat = require('gulp-concat');
 
-gulp.task('helloWorld', [], function(){
+gulp.task('helloWorld', [], function () {
 
-    console.log('helloworld');
+  console.log('helloworld');
 });
 
-gulp.task('electron-start', [], function(){
-  childProcess.spawn(electron, ['.'], { stdio: 'inherit' }); 
+gulp.task('electron-start', [], function () {
+  childProcess.spawn(electron, ['.'], {
+    stdio: 'inherit'
+  });
+
+});
+
+gulp.task('build-js', [], function () {
+  gulp.src('node_modules/angular/angular.min.js')
+    .pipe(concat('angular.min.js'))
+    .pipe(gulp.dest('libraries/angular'));
 
 });
