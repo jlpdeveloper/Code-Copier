91482f8191ca8ec7ed011c4095d0e6ac5b03f720
jlpdeveloper <pottsman482@gmail.com>
2017-01-24 20:10:39 -0600

    added comments, finished copy angular function

--- main.js
+++ main.js
@@ -8,44 +8,89 @@ const dialog = electron.dialog;
 const path = require('path');
 const url = require('url');
 const fs = require('fs');
-
+const fse = require('fs-extra');
+/**function to show dialog to open a directory. Returns directory to callback */
 exports.selectDirectory = function (callback) {
   dialog.showOpenDialog(mainWindow, {
     properties: ['openDirectory']
   }, selectedFiles => callback(selectedFiles));
 };
-
+/**
+ * function go read all directories from a root directory
+ */
 exports.getDirectoriesFromFolder = function (srcpath) {
   return fs.readdirSync(srcpath).filter(function (file) {
     return fs.statSync(path.join(srcpath, file)).isDirectory();
   });
 };
-
+/**
+ * public function to read  directory structure
+ */
 exports.getDirectoryStructure = function (srcpath) {
-  
+  //call private method of readDirectory
   return readDirectory(srcpath);
 };
-
-function readDirectory(srcpath){
+/**
+ * function to read information about a directory
+ * creates sub items about each subdirectory recursively
+ */
+function readDirectory(srcpath) {
+  //get all children of path sent in
   var children = fs.readdirSync(srcpath);
-  var folderStructure = [];
-   var item = {
-        name: srcpath.split('\\').pop(),
-        files: [],
-        directories: []
-    };
+  //create main item to return
+  var item = {
+    //name is name of directory split on "\"
+    name: srcpath.split('\\').pop(),
+    sourcepath: srcpath,
+    files: [],
+    directories: []
+  };
+  //loop thru all children
   for (child of children) {
-   
-    if(fs.statSync(path.join(srcpath, child)).isDirectory()){
-     item.directories.push(readDirectory(path.join(srcpath, child)));
-    }
-    else{
-        item.files.push(child);
+    //if its  directory, call this method again and add to directoreis of main item
+    if (fs.statSync(path.join(srcpath, child)).isDirectory()) {
+      item.directories.push(readDirectory(path.join(srcpath, child)));
+    } else {
+      //else push file name
+      item.files.push(child);
     }
   }
-  
+  //return item
   return item;
-}
+};
+
+exports.copyItemToDirectory = function (item, destination) {
+  copyItemToDirectory(item, destination);
+};
+/**
+ * function to recursively copy and make .bak files for everything
+ */
+function copyItemToDirectory(item, destination) {
+
+  // dir has now been created, including the directory it is to be placed in
+  //push all files to this directory
+  for (file of item.files) {
+    //cache destinationFile path. we use it alot
+    var destinationFilePath = path.join(destination, file);
+   
+    //check to see if file exists, if so, mke a bak file
+    if (fs.existsSync(destinationFilePath)) {
+      
+      fse.copySync(destinationFilePath, destinationFilePath + '.bak')
+    }
+    //copy new file in
+    fse.copySync(path.join(item.sourcepath, file), destinationFilePath);
+  }
+  //loop thru all directories and copy Item to directory again
+  for (directory of item.directories) {
+    fse.ensureDirSync(path.join(destination, directory.name));
+    
+      copyItemToDirectory(directory, path.join(destination, directory.name))
+    ;
+  }
+
+
+};
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -66,7 +111,7 @@ function createWindow() {
   }))
 
   // Open the DevTools.
- // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
--- renderer.js
+++ renderer.js
@@ -1,47 +1,79 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var fs = require('fs');
+//constants for electron,remote, and the main process
 const electron = require('electron');
 const remote = electron.remote
 const mainProcess = remote.require('./main');
-
-function createDirectoryLevel(directory, caller) {
-    console.log(directory);
-};
-
+const path = require('path');
+//define the angular module for this process
 angular.module("copier", []).controller("copyCtrl", function ($scope) {
+    //setup properties of controller
     var ctrl = this;
     ctrl.sourceFolder = "Select a source Folder to start";
     ctrl.destinationFolder = "";
     ctrl.hasSelectedSource = false;
     ctrl.hasSelectedDestination = false;
+    //source directory is an object, while destination is list of strings
     ctrl.sourceDirectory = {
         name: "",
         files: [],
         directories: []
     };
     ctrl.destinationDirectories = [];
-
+    /**
+     * function for selecting source directory
+     */
     ctrl.sourceOpenClick = function () {
-        mainProcess.selectDirectory(function (files) {
-            ctrl.sourceFolder = files[0];
+        //call open directory dialog function from main process
+        mainProcess.selectDirectory(function (folders) {
+            //get first folder from list and get structure 
+            ctrl.sourceFolder = folders[0];
             ctrl.hasSelectedSource = true;
             ctrl.sourceDirectory = mainProcess.getDirectoryStructure(ctrl.sourceFolder);
-
+            //update scope
             $scope.$apply();
         });
-
     };
-
+    /**
+     * function to get destination folder from dialog
+     */
     ctrl.destinationOpenClick = function () {
-        mainProcess.selectDirectory(function (files) {
-            ctrl.destinationFolder = files[0];
+        //call main process to open dialog
+        mainProcess.selectDirectory(function (folders) {
+            //select first folder from folders sent in
+            ctrl.destinationFolder = folders[0];
             ctrl.hasSelectedDestination = true;
+            //map sub directories from this main diretory
             ctrl.destinationDirectories = mainProcess.getDirectoriesFromFolder(ctrl.destinationFolder);
-            console.log(ctrl.destinationDirectories);
+            //update scope
             $scope.$apply();
         });
-
     };
+    /**
+     * function to copy from source to destination
+     */
+    ctrl.copyToDestination = function () {
+        //make sure source and destination have been selected
+        if (ctrl.hasSelectedSource) {
+            if (ctrl.hasSelectedDestination) {
+                //confirm user really wants to do this
+                if (confirm('Are you sure you wish to copy the source to all destinations?')) {
+                    //loop thru all directories in destination directory
+                    for (var directory of ctrl.destinationDirectories) {
+                        //call copy item from main process
+                        mainProcess.copyItemToDirectory(ctrl.sourceDirectory, path.join(ctrl.destinationFolder, directory));
+                    }
+                }
+                //alert user we're done
+                alert('Files have been propagated');
+            } else {
+                //else ask user to select destination
+                alert('Please select a destination');
+            }
+        } else {
+            //else ask user to select source
+            alert('Please select a source');
+        }
+    }
 });
