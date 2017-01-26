5521ef8575e7401922077220464838fed9b99fb6
jlpdeveloper <pottsman482@gmail.com>
2017-01-25 19:15:53 -0600

    Added top level only and ignore node_modules If destination is empty, we just copy to top level Added switch to check if we want to copy over node_modules or not

--- renderer.js
+++ renderer.js
@@ -14,6 +14,8 @@ angular.module("copier", []).controller("copyCtrl", function ($scope) {
     ctrl.destinationFolder = "";
     ctrl.hasSelectedSource = false;
     ctrl.hasSelectedDestination = false;
+    ctrl.top_level_destination_only = false;
+    ctrl.ignore_node_modules = true;
     //source directory is an object, while destination is list of strings
     ctrl.sourceDirectory = {
         name: "",
@@ -30,7 +32,7 @@ angular.module("copier", []).controller("copyCtrl", function ($scope) {
             //get first folder from list and get structure 
             ctrl.sourceFolder = folders[0];
             ctrl.hasSelectedSource = true;
-            ctrl.sourceDirectory = mainProcess.getDirectoryStructure(ctrl.sourceFolder);
+            ctrl.sourceDirectory = mainProcess.getDirectoryStructure(ctrl.sourceFolder, ctrl.ignore_node_modules);
             //update scope
             $scope.$apply();
         });
@@ -46,6 +48,10 @@ angular.module("copier", []).controller("copyCtrl", function ($scope) {
             ctrl.hasSelectedDestination = true;
             //map sub directories from this main diretory
             ctrl.destinationDirectories = mainProcess.getDirectoriesFromFolder(ctrl.destinationFolder);
+            //if this folder is empty, lets just say we want to push to top level only
+            if (ctrl.destinationDirectories.length == 0) {
+                ctrl.top_level_destination_only = true;
+            }
             //update scope
             $scope.$apply();
         });
@@ -59,10 +65,16 @@ angular.module("copier", []).controller("copyCtrl", function ($scope) {
             if (ctrl.hasSelectedDestination) {
                 //confirm user really wants to do this
                 if (confirm('Are you sure you wish to copy the source to all destinations?')) {
-                    //loop thru all directories in destination directory
-                    for (var directory of ctrl.destinationDirectories) {
+                    //if we want to copy to top level only, lets just copy to destination folder
+                    if (ctrl.top_level_destination_only) {
                         //call copy item from main process
-                        mainProcess.copyItemToDirectory(ctrl.sourceDirectory, path.join(ctrl.destinationFolder, directory));
+                        mainProcess.copyItemToDirectory(ctrl.sourceDirectory, ctrl.destinationFolder);
+                    } else {
+                        //loop thru all directories in destination directory
+                        for (var directory of ctrl.destinationDirectories) {
+                            //call copy item from main process
+                            mainProcess.copyItemToDirectory(ctrl.sourceDirectory, path.join(ctrl.destinationFolder, directory));
+                        }
                     }
                 }
                 //alert user we're done
@@ -74,6 +86,25 @@ angular.module("copier", []).controller("copyCtrl", function ($scope) {
         } else {
             //else ask user to select source
             alert('Please select a source');
+        }
+    }
+    /**
+     * function to reparse folder structure on ignore node modules change
+     */
+    ctrl.ignore_node_modules_change = function () {
+        //if a source has been selected, we make a temp object to tell user we're reparsing
+        if (ctrl.hasSelectedSource) {
+            ctrl.sourceDirectory = {
+                name: "",
+                files: ['Please Wait, Reparsing folder structure'],
+                directories: []
+            };
+             //update scope
+            $scope.$apply();
+            //reparse source folder
+            ctrl.sourceDirectory = mainProcess.getDirectoryStructure(ctrl.sourceFolder, ctrl.ignore_node_modules);
+            //update scope
+            $scope.$apply();
         }
     }
 });
