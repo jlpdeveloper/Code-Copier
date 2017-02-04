c2cb8dadd37b22dfdfc1c247b108291a89be4e52
jlpdeveloper <pottsman482@gmail.com>
2017-02-03 20:48:35 -0600

    Modified Build for new dependecy versions

--- gulpfile.js
+++ gulpfile.js
@@ -18,10 +18,10 @@ gulp.task('electron-start', ['build-js', 'build-css'], function () {
 
 gulp.task('build-js', [], function () {
   gulp.src([
-    'node_modules/angular/angular.min.js' 
- // 'node_modules/angular-aria/angular-aria.min.js',
- // 'node_modules/angular-animate/angular-animate.min.js',
- // 'node_modules/angular-material/angular-material.min.js'
+    'node_modules/angular/angular.min.js' ,
+  'node_modules/angular-aria/angular-aria.min.js',
+  'node_modules/angular-animate/angular-animate.min.js',
+  'node_modules/angular-material/angular-material.min.js'
   ])
    
     .pipe(concat('angular.min.js'))
